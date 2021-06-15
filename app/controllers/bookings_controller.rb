b1908349243c0708e075efbfea50aea119e5bf0b
MarieCichon <marie-chn@hotmail.fr>
2021-06-15 16:32:09 +0200

    methode new et create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,20 @@
 class BookingsController < ApplicationController
   def new
+    @van = Van.find(params[:van_id])
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @van = Van.find(params[:van_id])
+    @user = current_user
+    @booking.van = @van
+    @booking.user = @user
+    if @booking.save
+      redirect_to van_path(@van)
+    else
+      render :new
+    end
   end
 
   def accept
@@ -12,5 +24,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starting_date, :ending_date)
   end
 end
