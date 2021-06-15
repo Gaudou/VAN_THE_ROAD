9c815332a5d1e72707dc7ce1098b8657f1f909d8
Marie Leuliette <marie.leuliette@gmail.com>
2021-06-15 12:19:29 +0200

    creating homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def search(search)
+    if search
+      @vans = Van.where(model: search)
+    else
+      "#{search} do not meet any record"
+    end
+  end
 end
