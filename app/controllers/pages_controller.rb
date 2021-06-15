e4a8223d73df138349c997171bdb7ff4e1209fbc
Marie Leuliette <marie.leuliette@gmail.com>
2021-06-15 13:47:54 +0200

    creating navbar & footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,6 @@ class PagesController < ApplicationController
   def home
   end
 
-  def search(search)
-    if search
-      @vans = Van.where(model: search)
-    else
-      "#{search} do not meet any record"
-    end
+  def search
   end
 end
