60d335a9e906f1e42aa8edbff9fc116ec23b0e30
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-15 11:46:05 +0200

    New Page done - code clean - need to be tested

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -6,17 +6,32 @@ class VansController < ApplicationController
   end
 
   def new
+    @van = Van.new
   end
 
   def create
+    @van = Van.new(params_van)
+    @user = User.new
+    if @van.save
+      redirect_to van_path(@van)
+    else
+      render :new
+    end
   end
 
   def edit
+    @van = Van.new
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def params_van
+    params.require(:van).permit(:name, :model, :capacity, :van_city, :price_per_day)
   end
 end
