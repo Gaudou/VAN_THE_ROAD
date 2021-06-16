4692d8c4d2c1ba9776b03fced5b311a16b8ae0a0
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-16 16:10:47 +0200

    Delete Fixed : Dependent: :destroy add in van model

--- app/models/van.rb
+++ app/models/van.rb
@@ -1,6 +1,6 @@
 class Van < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :name, :model, :capacity, :van_city, :price_per_day, presence: true
 end
