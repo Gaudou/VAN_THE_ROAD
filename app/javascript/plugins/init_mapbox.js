bbce6fe8968f414a968adcb1ec42f305263065b0
MarieCichon <marie-chn@hotmail.fr>
2021-06-17 15:39:28 +0200

    map ajoutée avec logo van

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,46 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
+  const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundSize = 'contain';
+    element.style.backgroundRepeat = 'no-repeat';
+    element.style.width = '25px';
+    element.style.height = '25px';
+    element.innerHTML = '<i class="fas fa-shuttle-van" style="color:#d98526"></i>'
+    element.style.fontSize = "30px"
+      new mapboxgl.Marker(element)
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
+      .addTo(map);
+  });
+};
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+
+    fitMapToMarkers(map, markers);
+    addMarkersToMap(map, markers);
+  }
+};
+
+export { initMapbox };
--- app/models/van.rb
+++ app/models/van.rb
@@ -3,4 +3,6 @@ class Van < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :name, :model, :capacity, :van_city, :price_per_day, presence: true
+  geocoded_by :van_city
+  after_validation :geocode, if: :will_save_change_to_van_city?
 end
